dab6c2d1bc30b2665435cc525190844d4498dda1
mana629 <sayantibanerjee98@gmail.com>
2026-08-19 20:08:37 +0530

    Update tools.py with SYSTEM_PROMPT and agent loop

--- tools.py
+++ tools.py
@@ -1,4 +1,8 @@
 from datetime import datetime
+import warnings
+warnings.filterwarnings('ignore')
+
+from llm_client import get_llm_response
 
 def calculator(expression: str) -> str:
     """Evaluates a mathematical expression."""
@@ -22,3 +26,33 @@ TOOLS = {
     "get_weather": get_weather,
     "get_current_time": get_current_time
 }
+
+SYSTEM_PROMPT = """
+You are a helpful assistant that can answer questions and perform calculations. You have access to the following tools: 
+calculator - use it when you need to calculate something
+get_weather - use it when you need to get the weather of a city
+get_current_time - use it when you need to get the current time
+
+{
+  "tool" :  tool_name,
+   "input" : tool_input
+
+ }
+
+if no tool is needed , respond normally
+"""
+
+if __name__ == "__main__":
+    while True:
+        user_input = input("You: ")
+
+        if user_input.lower() == "quit":
+            break
+
+        messages = [
+            {'role': 'system', 'content': SYSTEM_PROMPT},
+            {'role': 'user', 'content': user_input}
+        ]
+
+        response = get_llm_response(messages)
+        print(f"bot: {response}")
